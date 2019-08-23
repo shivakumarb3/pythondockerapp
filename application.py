52051ed13f231a3593a6e5061c40460e7f799f32
Tadakamalla Shiva Kumar <shivakumar.thadakamalla@outlook.com>
2019-08-23 22:56:18 +0530

    Update application.py

--- application.py
+++ application.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World! docker app thu dinamma em halat aipoindi ra"
+    return "Hello World! docker app thu dinamma em halat aipoindi ra idi ravatle entro"
 if __name__ == "__main__":
     app.run(port=5000, debug=True, host='0.0.0.0')
